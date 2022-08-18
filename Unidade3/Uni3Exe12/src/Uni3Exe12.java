194bd5df10e12f0b71e3bf8d21ba88d5fd46e64d
guillermorodrigues276 <guillermorodrigues276@gmail.com>
2022-08-18 14:52:37 -0300

    uni3

--- Unidade3/Uni3Exe12/src/Uni3Exe12.java
+++ Unidade3/Uni3Exe12/src/Uni3Exe12.java
@@ -2,41 +2,53 @@ import java.util.Scanner;
 
 public class Uni3Exe12 {
     public static void main(String[] args) {
-
+        
         Scanner input = new Scanner(System.in);
-        
+
         String nome;
-        float numeroHorasTrabalhadas, salarioComDependente, salarioLiquido1, salarioLiquido2, decontoINSS, descontoIDR, calculoSalario, calculoSFamila, numeroDependentes, valorHora, valorDependente;
+        float horasTrabMes, numeroDependentes, valorSalario, salarioDependentes, salarioBruto, descontoINSS, descontoIDR, descontosIntegrados, salarioLiquido;
 
+        // NOME
+        System.out.println("Qual o nome do funcionario?");
         nome = input.next();
-        numeroHorasTrabalhadas = input.nextFloat();
+
+        // HORAS TRABALHADAS
+        System.out.println("Quantas hora mensais foram trabalhadas?");
+        horasTrabMes = input.nextFloat();
+
+        // DEPENDENTES
+        System.out.println("Quantos dependentes?");
         numeroDependentes = input.nextFloat();
-        valorHora = 10f;
-        valorDependente = 60f;
 
-        calculoSalario = (numeroHorasTrabalhadas*valorHora);
-        calculoSFamila = numeroDependentes*valorDependente;
+        // VALOR SALARIO
+        valorSalario = horasTrabMes*10;
 
-        salarioComDependente = calculoSalario+calculoSFamila;
+        // SALARIO DEPENDENTES
+        salarioDependentes = numeroDependentes*60;
 
+        // SALARIO BRUTO
+        salarioBruto = valorSalario+salarioDependentes;
 
-        decontoINSS = calculoSalario/0.085f;
-        descontoIDR = calculoSalario/0.05f;
+        // DESCONTOS
+        descontoINSS = salarioBruto*0.085f;
+        descontoIDR = salarioBruto*0.05f;
+        descontosIntegrados = descontoIDR+descontoINSS;
 
-        salarioLiquido1 = salarioComDependente+-decontoINSS+-descontoIDR;
+        // SALARIO LIQUIDO
+        salarioLiquido = salarioBruto-descontosIntegrados;
 
-        salarioLiquido2 = calculoSalario-salarioLiquido1;
+        // FIM
+        System.out.println("NOME " + nome);
+        System.out.println("SALARIO BRUTO: " + salarioBruto);
+        System.out.println("SALARIO LIQUIDO " + salarioLiquido);
 
-
-
-        System.out.println("NOME:"+ nome);
-        System.out.println("SALARIO BRUTO: "+calculoSalario);
-        System.out.println("SALARIO LOQUIDO: " + salarioLiquido2);
-
-
-
-
-        
         input.close();
+
+
+
+
+
+
+
     }
 }
