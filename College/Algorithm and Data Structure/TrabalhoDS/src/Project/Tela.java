3dd50b579f0a16dd9d61a6290ea8fa0e1329f9be
Guillermo Rodrigues <guillermorodrigues276@gmail.com>
2023-05-11 20:51:46 -0300

    update

--- College/Algorithm and Data Structure/TrabalhoDS/src/Project/Tela.java
+++ College/Algorithm and Data Structure/TrabalhoDS/src/Project/Tela.java
@@ -1,6 +1,5 @@
 package Project;
 
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -8,9 +7,8 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
-
-import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.GridLayout;
 
 public class Tela extends JFrame{
 
@@ -36,33 +34,49 @@ public class Tela extends JFrame{
             JLabel WriteOnBlackSpace = new JLabel("Arquivo bem formatado");
             WriteOnBlackSpace.setBounds(8,0,370,170);
             add(WriteOnBlackSpace);
+            WriteOnBlackSpace.setText("PEDRO PAULO"); //? Escrita pos verificação
 
             //! Painel em branco
             JPanel panel = new JPanel();
             panel.setBounds(8,50,370,130);
             add(panel);
 
-            //! Painel branco de baixo
+            //! Painel e Tabela
             JPanel panel2 = new JPanel();
+            panel2.setLayout(new GridLayout(1,1));
             panel2.setBounds(8,230,370,175);
+            JTable table = new JTable(dados,colunas);
+            JScrollPane scroll = new JScrollPane(table);
+            panel2.add(scroll);
             add(panel2);
-        
-
-            // JTable table = new JTable();
-            // JScrollPane spTable = new JScrollPane(table);     
-            // spTable.setBounds(8,230,350,170);
-            // table.setVisible(true);
-            // panel2.add(spTable);
             
+            //! Configuração JFrame
+            setTitle("HTML Scanner");
+            setSize(400,450);
+            setLocationRelativeTo(null);
+            setResizable(false);
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
+            getContentPane().setBackground(new Color(211,211,211));
+            setVisible(true);
+    };
 
-        setTitle("HTML Scanner");
-        setSize(400,450);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        getContentPane().setBackground(new Color(211,211,0));
-        setVisible(true);
-    }
+    Object [][] dados = {
+        {"html", "1"},
+        {"body", "2"},
+        {"p", "3"},
+        {"br", "4"}
+    };
+
+    String [] colunas = {"Tag", "Numeros de Ocorrências"};
+    
     public static void main(String[] args) {
         Tela tela = new Tela();
     }
 }
+
+
+//? @test fazer um scanner no blankSpace que vai ler o que for escrito e armazenar em uma variavel,
+//? após isso ele ira verificar no outro scanner se o arquivo escrito contem um .html, se tiver vai
+//? vai ser ser verificado pela biblioteca readArchive
+
+//? Ler arquivo com scanner, fazer verificação e achar um jeito de ver o arquivo .html
