a5c6c3aa351e2eb33fca8850860de8f822912950
Guillermo Rodrigues <guillermorodrigues276@gmail.com>
2023-08-16 23:04:41 -0300

    update

--- College/Object-Oriented Programming/Random/Random2/UsaConta.java
+++ College/Object-Oriented Programming/Random/Random2/UsaConta.java
@@ -11,27 +11,36 @@ public class UsaConta {
 
         String opt = "s";
         int opcao = 0;
+        boolean variavelBoo = true;
         
         while(opt != "n"){
 
-            System.out.print("Digite a sua escolha: ");
-            System.out.println();
-            System.out.println("1 - Depositar");
-            System.out.println("2 - Sacar");
-            System.out.println("3 - Saldo");
-            System.out.println("4 - Opções");
+            while(variavelBoo == true){
+                System.out.print("Digite a sua escolha: ");
+                System.out.println();
+                System.out.println("1 - Depositar");
+                System.out.println("2 - Sacar");
+                System.out.println("3 - Saldo");
+                System.out.println("4 - Opções");
 
             
-            opcao = input.nextInt();
+                opcao = input.nextInt();
             
-            if(opcao == 1){
-                System.out.println("Informe um valor para depositar: ");
-                double valorDeposito = input.nextDouble();
-                c1.depositar(valorDeposito);
-            }else if(opcao == 2){
-                System.out.println("Informe um valor para sacar: ");
-                double valorSaque = input.nextDouble();
-                c1.sacar(valorSaque);
+                if(opcao == 1){
+                    System.out.println("Informe um valor para depositar: ");
+                    double valorDeposito = input.nextDouble();
+                    c1.depositar(valorDeposito);
+                    variavelBoo = true;
+                }else if(opcao == 2){
+                    System.out.println("Informe um valor para sacar: ");
+                    double valorSaque = input.nextDouble();
+                    c1.sacar(valorSaque);
+                    variavelBoo = true;
+                }else if(opcao == 3){
+                    System.out.println("Saldo: " + c1.getSaldo());
+                    variavelBoo = true;
+
+                }
             }
             
             // switch(opcao){
