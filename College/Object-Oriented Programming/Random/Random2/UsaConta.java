3720fa424313183ded32809ead5c67a79099c181
Guillermo Rodrigues <guillermorodrigues276@gmail.com>
2023-08-16 23:45:57 -0300

    update

--- College/Object-Oriented Programming/Random/Random2/UsaConta.java
+++ College/Object-Oriented Programming/Random/Random2/UsaConta.java
@@ -9,64 +9,43 @@ public class UsaConta {
 
         Conta c1 = new Conta();
 
-        String opt = "s";
         int opcao = 0;
         boolean variavelBoo = true;
-        
-        while(opt != "n"){
 
-            while(variavelBoo == true){
-                System.out.print("Digite a sua escolha: ");
-                System.out.println();
-                System.out.println("1 - Depositar");
-                System.out.println("2 - Sacar");
-                System.out.println("3 - Saldo");
-                System.out.println("4 - Opções");
+        while(variavelBoo){
+            System.out.print("Digite a sua escolha: ");
+            System.out.println();
+            System.out.println("1 - Depositar");
+            System.out.println("2 - Sacar");
+            System.out.println("3 - Saldo");
+            System.out.println("4 - Sair do sistema");
 
-            
-                opcao = input.nextInt();
-            
-                if(opcao == 1){
+            opcao = input.nextInt();
+
+            switch(opcao) {
+                case 1:
                     System.out.println("Informe um valor para depositar: ");
                     double valorDeposito = input.nextDouble();
                     c1.depositar(valorDeposito);
                     variavelBoo = true;
-                }else if(opcao == 2){
+                break;
+
+                case 2:
                     System.out.println("Informe um valor para sacar: ");
                     double valorSaque = input.nextDouble();
                     c1.sacar(valorSaque);
                     variavelBoo = true;
-                }else if(opcao == 3){
+                break;
+
+                case 3:
                     System.out.println("Saldo: " + c1.getSaldo());
                     variavelBoo = true;
+                break;
 
-                }
+                case 4:
+                    System.out.println("Saindo do sistema");
+                    variavelBoo = false;
             }
-            
-            // switch(opcao){
-            //     case 1:
-            //         System.out.println("Informe um valor para depositar: ");
-            //         double valorDeposito = input.nextDouble();
-            //         c1.depositar(valorDeposito);
-            //     break;
-            //     case 2:
-            //         System.out.println("Informe um valor para sacar: ");
-            //         double valorSaque = input.nextDouble();
-            //         c1.sacar(valorSaque);
-            //     break;
-            //     case 3:
-            //         System.out.println("Saldo: " + c1.getSaldo());
-            //     break;        
-            //     case 4:
-            //         System.out.print("Digite a sua escolha: ");
-            //         System.out.println();
-            //         System.out.println("1 - Depositar");
-            //         System.out.println("2 - Sacar");
-            //         System.out.println("3 - Saldo");
-            //         System.out.println("4 - Opções");
-            //     break;
-            // }
-            opt = input.next();
         }
     }
 
